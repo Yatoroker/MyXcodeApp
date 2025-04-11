78adbefe79a782ac8de45f9c9fbafbbf25c21b90
ld <ld>
2025-04-11 22:45:56 +0800

    Initial Commit

---
+++ meritcounter2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  meritcounter2
+//
+//  Created by l d on 2025/4/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ meritcounter2/meritcounter2App.swift
@@ -0,0 +1,17 @@
+//
+//  meritcounter2App.swift
+//  meritcounter2
+//
+//  Created by l d on 2025/4/11.
+//
+
+import SwiftUI
+
+@main
+struct meritcounter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
